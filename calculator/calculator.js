e61c07b6b56de8671f01ab8e085c508c19d3eb4c
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-12-06 23:33:19 +0530

    Renew calculator project

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,7 +1,40 @@
-let calculation = localStorage.getItem('value') || '' ;
-let ans = document.querySelector('div');
-function fufu(value) {
-  calculation += value;
-  ans.innerText = calculation;
-  localStorage.setItem('value',calculation);
-}
+let equalPressed = 0;
+let buttonInput = document.querySelectorAll(".input-button");
+let input = document.querySelector("#input");
+let equal = document.querySelector("#equal");
+let clear = document.querySelector("#clear");
+let erase = document.querySelector("#erase");
+window.onload = () =>{
+  input.value = "";
+};
+buttonInput.forEach((buttonInput)=>{
+  buttonInput.addEventListener("click",()=>{
+    if(equalPressed == 1){
+      input.value = "";
+      equalPressed = 0;
+    }else{
+      input.value += buttonInput.value;
+    }
+  })
+})
+equal.addEventListener("click",()=>{
+  equalPressed = 1;
+  let inp_val = input.value;
+  try{
+    let solution = eval(inp_val)
+    if(Number.isInteger(solution)){
+      input.value = solution;
+    }else{
+      input.value = solution.toFixed(2);
+    }
+  }catch(err){
+    alert("Invalid Input");
+  }
+});
+clear.addEventListener("click",()=>{
+  input.value = "";
+});
+erase.addEventListener("click",()=>{
+  input.value = input.value.substr(0, input.value.lenght - 1);
+  // input.value = input.value.substr(0,input.value.lenght - 1);
+});
