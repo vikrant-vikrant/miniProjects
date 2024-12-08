2d0288630c5a2a1118a338b2fd4770578172e75f
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-12-08 18:19:37 +0530

    remove some extra code from catFact&dogImg

--- catFact&DogImg/app.js
+++ catFact&DogImg/app.js
@@ -1,46 +1,8 @@
-
-const EXCHANGE_RATE_API_URL = 'https://v6.exchangerate-api.com/v6/6bba2d089f3dda94675ef479/latest/inr';
-
-async function getExchangeRate(fromCurrency, toCurrency) {
-  try {
-    const response = await axios.get(EXCHANGE_RATE_API_URL);
-    const exchangeRates = response.data.conversion_rates;
-
-    const fromRate = exchangeRates[fromCurrency.toUpperCase()];
-    const toRate = exchangeRates[toCurrency.toUpperCase()];
-
-    if (!fromRate || !toRate) {
-      throw new Error(`Unable to find exchange rate for ${fromCurrency} or ${toCurrency}`);
-    }
-
-    const conversionRate = toRate / fromRate;
-    return conversionRate;
-  } catch (error) {
-    console.error('Error fetching exchange rate:', error.message);
-    throw error;
-  }
-}
-
-async function showConversion(fromCurrency, toCurrency) {
-  try {
-    const conversionRate = await getExchangeRate(fromCurrency, toCurrency);
-    console.log(`1 ${fromCurrency.toUpperCase()} = ${conversionRate.toFixed(2)} ${toCurrency.toUpperCase()}`);
-  } catch (error) {
-    console.error('Error displaying conversion:', error.message);
-  }
-}
-
-// Example usage:
-const fromCurrency = 'usd';
-const toCurrency = 'inr';
-showConversion(fromCurrency, toCurrency);
-
-
 //to use API
 const url3 = "https://catfact.ninja/fact";
 const data = async () =>{
     let response = await fetch(url3);
-    // console.log(response); //JSON format
+    console.log(response); //JSON format
     let data = await response.json();
     console.log(data);
 }
